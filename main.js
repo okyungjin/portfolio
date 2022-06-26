e9f490fa02daf8d7ce66e3cb9ccb54fc542b1bb9
okyungjin <okyungjin@gmail.com>
2022-06-26 22:27:00 +0900

    FEAT Make home slowly fade to transparent as window scrolls down

--- main.js
+++ main.js
@@ -37,3 +37,11 @@ const scrollToContactElem = () => {
   scrollIntoViewBySelector(contactElem);
 };
 homeContanctBtn.addEventListener('click', scrollToContactElem);
+
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
