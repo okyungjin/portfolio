09b7e64ea7d9ac5af4284c8d69e2723776ab37bc
okyungjin <okyungjin@gmail.com>
2022-06-26 22:42:29 +0900

    FEAT Show arrow up button when scrolling down

--- main.js
+++ main.js
@@ -45,3 +45,14 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+
+// Show arrow up button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2)
+    arrowUp.classList.add('visible');
+  else
+    arrowUp.classList.remove('visible');
+});
+arrowUp.addEventListener('click', () => { scrollIntoViewBySelector(home) });
